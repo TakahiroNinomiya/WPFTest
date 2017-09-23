35ab6b18bd5e321ee6a72611f80d0c5715b1417f
takahiro <takahiro12345@hotmail.co.jp>
2017-09-24 00:18:07 +0900

    Test

---
+++ WPFTestSol/WPFTestSol/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTestSol
+{
+
+
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private int _Number = 0;
+
+        /// <summary>
+        /// 番号
+        /// </summary>
+        public int Number
+        {
+            get { return _Number; }
+            set
+            {
+                _Number = value;
+                this.SetProperty(ref this._Number, value);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            field = value;
+            var h = this.PropertyChanged;
+            if (h != null) { h(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
+    }
+}
